05d5c84c704d590193f10bd400063ecd280b73ee
AllHailMegatron <ahmadrthomp@gmail.com>
2019-05-16 21:06:44 -0400

    Creating unique event listeners, using index.js file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+//Mouseover
+const nav = document.querySelector('nav')
+nav.addEventListener('mouseover', function(event) {
+    event.target.style.color = 'dodgerblue';
+
+    setTimeout(function() {
+        event.target.style.color = '';  
+    }, 500);
+}, false);
